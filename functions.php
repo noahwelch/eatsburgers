d8bb3b22f32cf228c343b84848390c9ba4dc1ed4
Noah Welch <noah@oohology.com>
2019-08-16 10:32:21 -0400

    Added initial project files

---
+++ functions.php
@@ -0,0 +1,84 @@
+<?php
+
+
+if (! function_exists('starts_with')) {
+    function starts_with($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle != '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string  $path
+     * @param  string  $manifestDirectory
+     * @return \Illuminate\Support\HtmlString
+     *
+     * @throws \Exception
+     */
+    function mix($path, $manifestDirectory = '')
+    {
+        static $manifests = [];
+
+        if (! starts_with($path, '/')) {
+            $path = "/{$path}";
+        }
+
+        if (!$manifestDirectory) {
+            $manifestDirectory = get_template_directory();
+        }
+
+        if ($manifestDirectory && ! starts_with($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+
+        if (file_exists(get_stylesheet_directory() . $manifestDirectory.'/hot')) {
+            return new HtmlString("//localhost:8080{$path}");
+        }
+
+        $manifestPath = $manifestDirectory . '/mix-manifest.json';
+
+        if (! isset($manifests[$manifestPath])) {
+            if (! file_exists($manifestPath)) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+
+            $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+        }
+
+        $manifest = $manifests[$manifestPath];
+
+        if (! isset($manifest[$path])) {
+            throw new Exception(
+                "Unable to locate Mix file: {$path}. Please check your ".
+                'webpack.mix.js output paths and try again.'
+            );
+        }
+
+        return (get_template_directory_uri() . $manifest[$path]);
+    }
+}
+
+function mix_enqueue_styles() {
+    $theme = wp_get_theme();
+    wp_enqueue_style(  'styles' , mix('/style.css'), [], null );
+}
+add_action( 'wp_enqueue_scripts', 'mix_enqueue_styles' );
+
+function mix_enqueue_scripts() {
+
+    $theme = wp_get_theme();
+    wp_enqueue_script( $theme->get('TextDomain') . '-jquery' , 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' , array() , '3.3.1' , true );
+
+    wp_enqueue_script( 'scripts' , mix('/js/app.js'), [], null );
+}
+
+add_action( 'wp_enqueue_scripts', 'mix_enqueue_scripts' );
---
+++ webpack.mix.js
@@ -0,0 +1,53 @@
+const configuration = require('./config.json')
+
+const path = require('path')
+const mix = require('laravel-mix')
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+const standardConfig = {
+    resolve: {
+        modules: [
+            path.resolve('./node_modules'),
+            path.resolve('./src/js'),
+        ],
+    },
+}
+
+const standardMix = () => mix
+        .scripts(['src/js/app.js'], './js/app.js')
+        .sass('src/scss/style.scss', './')
+        .disableNotifications()
+        .options({
+            processCssUrls: false
+        })
+        .setPublicPath('./')
+
+if (!mix.inProduction()) {
+    mix.webpackConfig(Object.assign({}, standardConfig))
+
+    standardMix()
+        .sourceMaps()
+        .browserSync({
+            proxy: configuration.APP_URL,
+            files: [
+                './js/**/*.js',
+                './**/*.html',
+                './**/*.php',
+                './*.css'
+            ]
+        })
+} else {
+    mix.webpackConfig(standardConfig)
+
+    standardMix().version()
+}
