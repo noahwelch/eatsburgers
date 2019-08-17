2762e1223c2ad9897cca24c5d9360a1aa3492625
Noah Welch <noah@oohology.com>
2019-08-17 11:08:10 -0400

    Added gencon post single and front page

---
+++ front-page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+Front Page
+
+<?php get_footer(); ?>
---
+++ page_gencon.php
@@ -0,0 +1,16 @@
+<?php
+    /*
+     * Template Name: Gencon Page
+    */
+
+?>
+
+<?php
+get_header();
+?>
+
+<?php the_title(); ?>
+
+<?php
+get_footer();
+?>
