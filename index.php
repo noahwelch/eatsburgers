825facfe42eed03f4ff88dd5c33495fe4f9fa42f
Noah Welch <noah@oohology.com>
2019-08-17 11:07:49 -0400

    Added loop to blog posts

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 
-Eats Burgers
+<?php while( have_posts() ): the_post(); ?>
+    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+    <?php the_time( 'F j, Y' ); ?>
+    <a class="button" href="<?php the_permalink(); ?>">Read This</a>
+<?php endwhile; ?>
 
-<?php
-get_footer();
-?>
+<?php get_footer(); ?>
